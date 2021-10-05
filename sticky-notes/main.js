55745404b16b18eae70af7ac67a7063e4b5fdf3c
Taha <taha_shahid@outlook.com>
2021-10-05 22:47:37 +0500

    initail commit

---
+++ sticky-notes/main.js
@@ -0,0 +1,12 @@
+const noteContainer = document.getElementById("app");
+const addNoteContainer = noteContainer.querySelector(".add-note");
+function getNotes() {}
+
+function saveNotes(notes) {}
+
+function createNoteElement(id, content) {}
+
+function addNote() {}
+
+function updateNote(id, newContent) {}
+function deleteNote(id) {}
